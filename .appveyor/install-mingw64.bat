943d2076db9ceea93dc64aa3a480a90b0891dd6e
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-04-16 12:58:46 +0200

    fix mingw64 scons installation

--- .appveyor/install-mingw64.bat
+++ .appveyor/install-mingw64.bat
@@ -1,14 +1,19 @@
 %SHELL% "echo 'cd %APPVEYOR_BUILD_FOLDER%' | sed -e 's/^\\(\\w\\):/\\/\\l\\1/' -e 's/\\\\/\\//g' | tee -a ~/.profile"
 %SHELL% "echo 'PATH=~/.local/bin:$PATH' | tee -a ~/.profile"
 
+REM Remove all unwanted packages
+%SHELL% "pacman -Qqs '(^mingw-w64-|^scons$|^python$|^python2$|^python3$)' | sort | uniq | xargs pacman -Rcs --noconfirm"
+
+REM Upgrade remaining packages
+%SHELL% "pacman -Syu --disable-download-timeout --noconfirm"
+%SHELL% "pacman -Syu --disable-download-timeout --noconfirm"
+
+REM Install what's necessary
 %SHELL% "pacman -Sy --disable-download-timeout --noconfirm --needed %PY% %PY%-pip"
 IF [%PY%]==[python2] (
-  %SHELL% "pacman -Rcs --noconfirm python3 mingw-w64-x86_64-python3"
-  %SHELL% "%PY% -m pip install --upgrade pip"
+  %SHELL% "test -e /usr/bin/python || ln -s /usr/bin/python2 /usr/bin/python"
   %SHELL% "%PY% -m pip install -r requirements2-dev.txt"
 ) ELSE (
-  %SHELL% "pacman -Rcs --noconfirm python2 mingw-w64-x86_64-python3"
-  %SHELL% "%PY% -m pip install --upgrade pip"
   %SHELL% "%PY% -m pip install -r requirements3-dev.txt"
 )
 %SHELL% "%PY% -m pip install -r requirements.txt"
